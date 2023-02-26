5e4f31516fd7960982c54a852200296f720da9e5
eChallenge <echallenge0x7e6@gmail.com>
2023-02-26 12:22:26 +0900

    2023/02/26_takao:Working for Drag&Drop

--- www/script.js
+++ www/script.js
@@ -47,3 +47,41 @@ async function sendSerial()
 	await writer.write(encoder.encode(text + "\n"));
 	writer.releaseLock();
 }
+
+let elImg = new Image();
+elImg.src = "./img/panel.png";
+
+function dragstart_handler(ev) {
+	// 対象となる要素の id を DataTransfer オブジェクトに追加する
+	ev.dataTransfer.effectAllowed = "move";
+	ev.dataTransfer.setData("application/my-app", ev.target);
+	console.log(ev.target);
+	ev.dataTransfer.setDragImage(elImg,0,0);
+	console.log("Drag!!!!!!!!!!!!");
+}
+function dragover_handler(ev) {
+	ev.preventDefault();
+	//ev.dataTransfer.dropEffect = "move";
+}
+function drop_handler(ev) {
+	ev.preventDefault();
+	// 移動された要素の id を取得して、その要素を target の DOM に追加する
+	const data = ev.dataTransfer.getData("application/my-app");
+	ev.target.appendChild(data);
+	console.log("Drop##########");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	// Get the element by id
+	const elements = document.getElementsByClassName("parts");
+	// Add the ondragstart event listener
+	for(let i=0; i<elements.length; i++){
+		elements[i].setAttribute("draggable","true");
+		elements[i].ondragstart = dragstart_handler;
+		//elements[i].addEventListener("dragstart", function ( event ) { event.dataTransfer.setData( "text", "SYNCER" ) ; });
+		//console.log(elements[i]);
+	}
+	const field = document.getElementById("fieldArea");
+	field.addEventListener("dragover", dragover_handler);
+	field.addEventListener("drop", drop_handler);
+});
